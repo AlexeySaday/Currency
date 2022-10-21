a615f67e8ac267015f6b67fcdee314d1238808c5
Alex Saday <alexeykildyushev@yandex.ru>
2022-10-21 23:11:39 +0300

    first commit

---
+++ ForArtem/Currency.cs
@@ -0,0 +1,26 @@
+﻿using System.Xml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ForArtem
+{
+    static class Currency
+    {
+        static string date;
+        static string url;
+        static Currency()
+        {
+            date = DateTime.Now.Add(TimeSpan.FromDays(1)).ToString("dd/MM/yyyy");
+            url = @"http://www.cbr.ru/scripts/XML_dynamic.asp?date_req1=" + date + "&date_req2=" + date + "&VAL_NM_RQ=";
+        }
+        static public string FubricOfCurrency(string currency)
+        {
+            return Fabrica.GetCurrency(currency,url,date);
+        } 
+    }
+     
+}  
---
+++ ForArtem/Fabrica.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Threading.Tasks;
+
+namespace ForArtem
+{
+    static class Fabrica
+    {
+        public static string GetCurrency(string valut,string url,string date)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            XmlTextReader reader;
+            switch (valut)
+            {
+                case "Usd":
+                    reader = new XmlTextReader(url + "R01235");
+                    break;
+                case "Eur":
+                    reader = new XmlTextReader(url + "R01239");
+                    break;
+                default: return "Возможно вы имели ввиду что то другое";
+            } 
+            int i = 0;
+            while (reader.Read())
+            {
+                i++;
+                if (i == 8)
+                    return reader.Value;
+            }
+            return "Похоже, что-то не так";
+        }
+    }
+}
